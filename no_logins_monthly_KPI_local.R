f58bb2b500d7528e6506923ddf91577c8a0f9857
Jung Mee Park <jpark1917@gmail.comecho>
2022-04-05 09:49:46 -0700

    from aws

---
+++ no_logins_monthly_KPI_local.R
@@ -0,0 +1,264 @@
+# no login user counts
+# Jung Mee Park
+# jmpark@arizona.edu
+# 2022-03-28
+
+####Load Libraries####
+library('tidyverse')
+library('dplyr')
+library(dbplyr)
+library('salesforcer')
+library('lubridate')
+library(reshape2)
+library(readxl)
+
+####check working directory####
+getwd() #figure out the working directory
+setwd("D:/Users/jmpark/Box/Trellis/Program Team/Trellis Metrics Reports/Trellis KPIs/Raw data")
+
+rm(list=ls(all=TRUE)) 
+options(digits=3)
+
+####capture current month####
+today <- Sys.Date()
+this_month <- month(today)
+last_month<-month(today() - months(1))
+this_year <- year(today)
+# last_year <-year(today() - years(1))
+
+format(today, format="%B %d %Y")
+#### function to write copy files into csv####
+write_named_csv <- function(x) 
+  write_csv(x, file = paste0("D:/Users/jmpark/WorkSpaces/jmpark_data/Program Team KPIs/Feb 2022 data files/",
+                             deparse(substitute(x)), last_month, this_year,".csv"))
+
+####Import data from SF####
+sf_auth()
+###bring in Contact Records
+my_soql_contact <- sprintf("SELECT Id,	Emplid__c, Email, User__c, Primary_Department__r.Name, 
+                           NetId__c, hed__Primary_Organization__c, MDM_Primary_Type__c
+                           FROM Contact")
+
+contact_records <- sf_query(my_soql_contact, object_name="Contact", api_type="Bulk 1.0")
+
+###bring in User object fields
+soql_users<-sprintf("select Id, Email, UserType, Name, NetID__c, Profile.Name, ContactId, CreatedDate,
+                    Department, ProfileId, UserRoleId, 
+                    UserRole.Name, Title, Username 
+                    from User
+                    WHERE IsActive=TRUE ")
+users_SF <- sf_query(soql_users, object_name="User", api_type="Bulk 1.0")
+
+###Bring In User Login History
+# solq_logins<-sprintf("Select UserId, Browser, LoginType, Platform, Status from LoginHistory")
+# user_logins <- sf_query(solq_logins, object_name="LoginHistory", api_type="Bulk 1.0")
+
+###bring in affiliation Records
+my_soql_aff <- sprintf("SELECT Academic_Department__c, hed__Affiliation_Type__c, hed__Contact__c, 
+                        hed__Primary__c, hed__Account__r.Name, Parent_Organization__c
+                           FROM hed__Affiliation__c")
+
+affiliations <- sf_query(my_soql_aff, object_name="hed__Affiliation__c", api_type="Bulk 1.0")
+
+# solq_logins<-sprintf("Select UserId, Browser, LoginTime, LoginType, Platform, Status from LoginHistory
+#                      WHERE LoginTime > 2022-01-01T00:00:00.000Z")
+# user_logins <- sf_query(solq_logins, object_name="LoginHistory", api_type="Bulk 1.0")
+
+
+###bring in permissionsets Records
+solq_perms<-sprintf("select AssigneeId, PermissionSet.Name, PermissionSet.Type, PermissionSet.ProfileId, 
+                    PermissionSetGroupId from PermissionSetAssignment")
+permissionsets <- sf_query(solq_perms, object_name="PermissionSetAssignment", api_type="Bulk 1.0")
+
+#### perms2prods ####
+perms2prods <- read_excel("20220224_Permissionsets_mapped_to_Products.xlsx")
+
+#### splunk reports ####
+splunk_reports <- list.files("./2022 Splunk Reports")  # Identify file names
+splunk_reports
+
+Social_Logins <- read_csv(paste0(this_year, " Splunk Reports/", month.name[last_month],
+                                 " ",this_year,"/Users,_last_login,_social_studio-",
+                                 this_year,"-0",this_month,"-01.csv"))
+# Users,_last_login,_marketing_cloud-2022-03-01
+MC_Logins <- read_csv(paste0(this_year, " Splunk Reports/", month.name[last_month],
+                             " ",this_year,"/Users,_last_login,_marketing_cloud-",
+                             this_year,"-0",this_month,"-01.csv"))
+
+#rename var indicating that login was for a particular tool
+MC_Logins$Profile.Name<-"MC"
+Social_Logins$Profile.Name<-"Social"
+
+colnames(MC_Logins)[1]<-"NetID__c"
+colnames(Social_Logins)[1]<-"NetID__c"
+
+# users <- read.csv("./2022 Splunk Reports/February 2022/MtD Licensed Users Logged In-2022-02-25-17-00-06.csv")
+
+####STEP 3: Merge files####
+# from Frances
+names(users_SF)
+
+users<-users_SF[,-c(5,8:12)] # remove email,"ProfileId", "Title", "Username","UserRoleId","UserType" 
+users<-unique(users)
+
+names(users)
+names(permissionsets)
+names(perms2prods)
+perms2prods<-perms2prods[,-c(2)] # drop 'n'
+
+users_perms<-merge(users, permissionsets, by.x = "Id", by.y = "AssigneeId")
+users_perms_prods<-merge(users_perms, perms2prods, by.x = "PermissionSet.Name", by.y = "PermissionSet.Name", all.x = TRUE)
+names(users_perms_prods)
+
+upp_c<-merge(users_perms_prods, contact_records, by.x = "Id", by.y = "User__c", all.x = TRUE)
+names(upp_c)
+
+upp_c %>% count(Profile.Name)
+
+upp_c<-distinct(upp_c)
+# upp_c<-subset(upp_c, upp_c$PermissionSet.Name!="X00ef4000001uSgMAAU" & upp_c$PermissionSet.Name!="X00e2S000000G6SIQA0")
+
+#### create 'base' product from profile name ####
+upp_c$Product[upp_c$UserRole.Name=="Data Analyst" & is.na(upp_c$Product)]<-"Reports"
+upp_c$Product[(upp_c$UserRole.Name=="SAFER Volunteer" | upp_c$UserRole.Name=="SAFER") & is.na(upp_c$Product)]<-"SAFER"
+upp_c$Product[upp_c$Profile.Name=="Advising Base" & is.na(upp_c$Product)]<-"Scheduling/Notes"
+upp_c$Product[upp_c$Profile.Name=="Service Desk Base" & is.na(upp_c$Product)]<-"Service Desk"
+upp_c$Product[upp_c$UserRole.Name=="Marketer" & is.na(upp_c$Product)]<-"Marketing - SF"
+
+upp_c2<-unique(upp_c)
+
+# upp_c2<-subset(upp_c2, !is.na(upp_c2$Product)) # consider taking this out
+
+####MC_s_c_u creation####
+# subset contact records
+contact_records <- subset(contact_records, !is.na(contact_records$NetID__c)) # 337326
+
+# merge MC contacts
+MC_contacts <- merge(contact_records, MC_Logins, by.x = "NetID__c", 
+                     by.y = "NetID__c", all = TRUE)
+
+MC_contacts_social <- merge(MC_contacts, Social_Logins, by.x = c("NetID__c"), 
+                            by.y = c("NetID__c"), all = TRUE)
+MC_s_c_u <- merge(MC_contacts_social, upp_c, by.x = c("NetID__c"), 
+                  by.y = c("NetID__c.y"), all=TRUE)
+
+names(MC_s_c_u) # marketing social contacts users
+
+# create a single column for Trellis Products
+product_longer <- MC_s_c_u %>% 
+  pivot_longer(cols = c(Profile.Name.x, Profile.Name.y, Product),
+               names_to = "col_name", 
+               values_to = "MC-Social-SFProduct") %>% 
+  drop_na("MC-Social-SFProduct") %>% 
+  distinct()
+
+names(product_longer)
+
+MC_s_c_u2 <- product_longer %>%
+  select(NetID__c,Name,UserRole.Name, Profile.Name,
+         "MC-Social-SFProduct", MDM_Primary_Type__c.x, PermissionSet.Name, 
+         hed__Primary_Organization__c.x, Department,Id.x,  
+         Primary_Department__r.Name.y) %>% 
+  distinct()
+
+MC_s_c_u2 %>% 
+  distinct(NetID__c, `MC-Social-SFProduct`) %>%
+  group_by(`MC-Social-SFProduct`) %>% 
+  count()%>% 
+  ungroup()
+#Merge in Affiliations
+names(affiliations)
+affiliation1s <- subset(affiliations, affiliations$hed__Primary__c==TRUE)
+affiliation1s <- affiliation1s[, -c(4)] # remove "hed__Primary__c"   
+affiliation1s<-distinct(affiliation1s)
+
+df_foruse <- merge(MC_s_c_u2, affiliation1s, by.x = "Id.x", by.y = "hed__Contact__c", all.x = TRUE)
+
+# distinct tables with select variables after merged into affiliations
+df_foruse2 <- df_foruse %>%
+  select(NetID__c,Name,UserRole.Name, Profile.Name,
+         "MC-Social-SFProduct",  PermissionSet.Name, 
+         Parent_Organization__c, hed__Account__r.Name) %>% 
+  distinct()
+
+write_named_csv(df_foruse2)
+
+# count unique
+df_foruse2 %>% 
+  distinct(NetID__c, `MC-Social-SFProduct`) %>%
+  group_by(`MC-Social-SFProduct`) %>% 
+  count()%>% 
+  ungroup()
+
+# figure out how many profile names there are
+# count unique
+df_foruse2 %>% 
+  distinct(NetID__c, Profile.Name) %>%
+  group_by(Profile.Name) %>% 
+  count()%>% 
+  ungroup()
+
+
+# filter based on profile name
+library(dplyr)
+target <- c("Marketing User", "System Administrator", "System User", 
+            "Trellis API Read Only", "Trellis User")
+
+df_fortable <- filter(df_foruse2, !Profile.Name %in% target)  # equivalently, dat %>% filter(name %in% target)
+df_fortable %>% 
+  distinct()
+
+write_named_csv(df_fortable)
+
+# user role name
+df_fortable %>% 
+  distinct(NetID__c, Profile.Name) %>%
+  group_by(Profile.Name) %>% 
+  count()%>% 
+  ungroup()
+
+df_fortable %>% 
+  distinct(NetID__c, Profile.Name) %>%
+  # group_by(Profile.Name) %>% 
+  count() # 1301
+# ungroup()
+df_fortable %>% 
+  distinct(NetID__c, `MC-Social-SFProduct`) %>%
+  group_by(`MC-Social-SFProduct`) %>% 
+  count()%>% 
+  ungroup()
+
+# target2 <- c("Advising Base", "Salesforce Base", "Service Desk Base")
+# NA_fortable <- filter(df_fortable, !Profile.Name %in% target2)  # equivalently, dat %>% filter(name %in% target)
+mtcars %>% 
+  group_by(cyl) %>% 
+  mutate(mean_mpg = mean(mpg),
+         mpg_diff = mpg-mean_mpg) %>% 
+  arrange()
+
+mtcars %>% 
+  group_by(cyl) %>% 
+  mutate(n = n()) %>% 
+  nest(-num) %>% 
+  slice_min(num, n=1) %>% 
+  unnest()
+
+expand_grid(mean=1:3, sd=1:3) %>% 
+  rowwise() %>% 
+  mutate(data = list(rnorm(100, mean = mean, sd = sd)))
+unnest() %>% 
+  ggplot(aes(x = data)) +
+  geom_histogram() +
+  facet_grid(rows = vars(mean), cols = vars(sd))
+
+# 5 rows from every category
+storms %>% 
+  group_by(category) %>% 
+  slice_sample(n=5)
+
+# samples of whole groups
+storms %>% 
+  group_by(category) %>% 
+  nest() %>% 
+  ungroup() %>% 
+  slice_sample(n=5)
